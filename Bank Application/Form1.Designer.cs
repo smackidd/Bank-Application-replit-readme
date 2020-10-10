2c2ca3848f38604271b69450a88e277b1528fafe
Steve Mackidd <s.mackidd425@mybvc.ca>
2020-10-10 14:12:30 -0600

    Updated Login Form GUI

--- Bank Application/Form1.Designer.cs
+++ Bank Application/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Bank_Application
 {
-    partial class Form1
+    partial class formLogin
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,83 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(formLogin));
+            this.lblBankTitleLogin = new System.Windows.Forms.Label();
+            this.tbUsername = new System.Windows.Forms.TextBox();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblBankTitleLogin
+            // 
+            this.lblBankTitleLogin.AutoSize = true;
+            this.lblBankTitleLogin.BackColor = System.Drawing.Color.CadetBlue;
+            this.lblBankTitleLogin.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblBankTitleLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblBankTitleLogin.Font = new System.Drawing.Font("Modern No. 20", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBankTitleLogin.Location = new System.Drawing.Point(23, 48);
+            this.lblBankTitleLogin.Name = "lblBankTitleLogin";
+            this.lblBankTitleLogin.Padding = new System.Windows.Forms.Padding(19, 81, 19, 81);
+            this.lblBankTitleLogin.Size = new System.Drawing.Size(365, 200);
+            this.lblBankTitleLogin.TabIndex = 0;
+            this.lblBankTitleLogin.Text = "Bank of Prometheus";
+            // 
+            // tbUsername
+            // 
+            this.tbUsername.Font = new System.Drawing.Font("Franklin Gothic Medium", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbUsername.Location = new System.Drawing.Point(402, 80);
+            this.tbUsername.Name = "tbUsername";
+            this.tbUsername.Size = new System.Drawing.Size(169, 31);
+            this.tbUsername.TabIndex = 1;
+            this.tbUsername.Text = "username";
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.Font = new System.Drawing.Font("Franklin Gothic Medium", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbPassword.Location = new System.Drawing.Point(402, 135);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.Size = new System.Drawing.Size(169, 31);
+            this.tbPassword.TabIndex = 2;
+            this.tbPassword.Text = "password";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.CadetBlue;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Franklin Gothic Medium Cond", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.Control;
+            this.button1.Location = new System.Drawing.Point(402, 206);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(168, 40);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // formLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(600, 306);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tbPassword);
+            this.Controls.Add(this.tbUsername);
+            this.Controls.Add(this.lblBankTitleLogin);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "formLogin";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblBankTitleLogin;
+        private System.Windows.Forms.TextBox tbUsername;
+        private System.Windows.Forms.TextBox tbPassword;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Bank Application/Form1.cs
+++ Bank Application/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Bank_Application
 {
-    public partial class Form1 : Form
+    public partial class formLogin : Form
     {
-        public Form1()
+        public formLogin()
         {
             InitializeComponent();
         }
